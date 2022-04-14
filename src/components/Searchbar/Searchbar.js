e5cd1053b097ba94f3208da7dc9b832fdf2c1d7e
AleksandrVasilko <avasilko7@gmail.com>
2022-04-14 09:58:44 +0300

    edit component Searchbar from Class to Hooks

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,48 +1,46 @@
-import { Component } from "react/cjs/react.production.min"
+import { useState } from "react"
 import { FcSearch} from 'react-icons/fc'
 import { toast } from 'react-toastify'
 
-class Searchbar extends Component{
-    state = {
-        searchValue: '',
+export default function Searchbar({ formSubmit }){
+    
+    const [searchValue, setSearchValue] = useState('');
+  
+    const handleChange = (e) => {
+        setSearchValue(e.currentTarget.value);
     };
 
-    handleChange = e => {
-        this.setState({ searchValue: e.currentTarget.value });
-    };
-
-    handleSubmit = e => {
-        const { searchValue } = this.state;
+    const handleSubmit = (e) => {
+        
         e.preventDefault();
         
         if (!searchValue.trim()) {
             toast.warn('Please enter what are you looking for');
             return;
         }
-        this.props.formSubmit(searchValue);
+        formSubmit(searchValue);
+        setSearchValue('')
     };
 
-    render() {
-        return (
-            <header className="Searchbar">
-                <form className="SearchForm" onSubmit={this.handleSubmit}>
-                    <button type="submit" className="SearchForm-button">
-                        <FcSearch style={{ with: 25, height: 25 }} />
-                    </button>
 
-                    <input
-                        className="SearchForm-input"
-                        type="text"
-                        autocomplete="off"
-                        autofocus
-                        placeholder="Search images and photos"
-                        value={this.setState.searchValue}
-                        onChange={this.handleChange}
-                    />
-                </form>
-            </header>
-        );
-    }
+    
+    return (
+        <header className="Searchbar">
+            <form className="SearchForm" onSubmit={handleSubmit}>
+                <button type="submit" className="SearchForm-button">
+                    <FcSearch style={{ with: 25, height: 25 }} />
+                </button>
+
+                <input
+                    className="SearchForm-input"
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                    value={searchValue}
+                    onChange={handleChange}
+                />
+            </form>
+        </header>
+    );
 }
-
-export default Searchbar;
